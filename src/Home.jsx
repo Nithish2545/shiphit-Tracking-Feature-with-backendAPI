d99f7e1b1ea012a513c05300760c7346c968701a
NithishKumarFreelancer <nithishkumarfreelancer@gmail.com>
2024-08-16 18:53:13 +0530

    figma url add to home.jsx for reference!

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,11 @@
 function Home() {
   return (
     <div>Home</div>
+
   )
 }
 
 export default Home
+
+
+// https://www.figma.com/design/G5jy8J8WDDuYbbzle0NsRU/Delivery-Service-Dashboard---Admin-Panel-(Community)?t=YFAoU3mzEpW8Movb-0
