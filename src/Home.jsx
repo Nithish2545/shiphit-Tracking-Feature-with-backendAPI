5eca2803b615b3b826d31efbec0486f01bc39e2c
NithishKumarFreelancer <nithishkumarfreelancer@gmail.com>
2024-08-18 21:06:04 +0530

    Tracking ui model is developed!

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,67 @@
+import "./Home.css";
 function Home() {
+  const Trackingdata = [
+    {
+      imageURL: "/circle1.svg",
+      text1: "Package has left Courier Facility",
+      text2: "San Francisco, California",
+    },
+    {
+      imageURL: "/circle1.svg",
+      text1: "Package arrived at Local Facility",
+      text2: "NEW YORK CITY, NEW YORK",
+    },
+    {
+      imageURL: "/circle3.svg",
+      text1: "Out for Delivery",
+      text2: "NEW YORK CITY, NEW YORK",
+    },
+    {
+      imageURL: "/circle2.svg",
+      text1: "Delivered",
+      text2: "1567 DOVE STREET, NEW YORK CITY, 9888",
+    },
+  ];
   return (
-    <div>Home</div>
+    <div className="home">
+      <h1 className="font-bold text-2xl">Tracking</h1>
+      <div className="bg-secondary w-fit rounded-xl">
+        <div className="p-4 flex flex-col gap-3">
+          <h1 className="font-bold text-xl text-white">#127777489-DL-NY</h1>
+          <div className="">
+            <button className="text-base bg-black text-white p-2 pl-5 pr-5 rounded-full">
+              Out for Delivery
+            </button>
+            <button className="text-base bg-black text-white p-2 pl-5 pr-5 rounded-full">
+              Parcel
+            </button>
+          </div>
+        </div>
+<div className="relative">
+<img src="HorizontalLine.svg" alt="" />
+<img src="verticalLine.svg" className="relative top-3" alt="" />
 
-  )
+        {Trackingdata.map((d) => (
+          <div className="flex gap-2">
+            <div className="w-9 flex justify-center">
+              <img
+                src={d.imageURL}
+                className={d.imageURL != "/circle3.svg" ? "w-5 h-5" : "w-8 h-8"}
+                alt=""
+              />
+            </div>
+            <div className="flex flex-col gap-2">
+              <p className="font-semibold text-sm text-white">{d.text1}</p>
+              <p className="font-normal text-xs uppercase text-white">
+                {d.text2}
+              </p>
+            </div>
+          </div>
+        ))}
+</div>
+
+      </div>
+    </div>
+  );
 }
-
-export default Home
-
-
-// https://www.figma.com/design/G5jy8J8WDDuYbbzle0NsRU/Delivery-Service-Dashboard---Admin-Panel-(Community)?t=YFAoU3mzEpW8Movb-0
+export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,30 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'sm': '640px',    // Small screens and up
+        'tb': '768px',    // Tablet screens and up
+        'lp': '1024px',   // Laptop screens and up
+        'lg': '1280px',   // Large screens and up
+      },
+      colors: {
+        primary: '#111111',   // Dark color
+        secondary: '#C3B649', // Gold-like color
+        tertiary: '#F2EED1',  // Light beige color
+        quaternary: '#F2EED1',// Same as tertiary for consistency
+        white: '#FFFFFF',     // White color
+      },
+      fontSize: {
+        'xs': '12px',  // Extra small text
+        'sm': '14px',  // Small text
+        'base': '18px',// Base text
+        'lg': '20px',  // Large text
+        'xl': '26px',  // Extra large text
+        '2xl': '32px', // 2x Extra large text
+        '3xl': '48px', // 3x Extra large text
+      },
+    },
   },
   plugins: [],
 }
-
